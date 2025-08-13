32a42b85659e43d95b50aba31cc5eb40054881bd
kaio <kaiomudkt@gmail.com>
2025-08-13 20:01:21 -0300

    salvando categoria no postgres

---
+++ dotnet/app_dotnet/Models/CategoryRequest.cs
@@ -0,0 +1,5 @@
+namespace app_dotnet.Models;
+
+// using app_dotnet.Models.Models.CategoryModel;
+
+public record CategoryRequest(string Name);
--- dotnet/app_dotnet/Routes/CategoryRoute.cs
+++ dotnet/app_dotnet/Routes/CategoryRoute.cs
@@ -1,4 +1,5 @@
 using app_dotnet.Models;
+using app_dotnet.Data;
 
 namespace app_dotnet.Routes;
 
@@ -7,6 +8,13 @@ public static class CategoryRoute
 
     public static void CategoryRoutes(WebApplication app)
     {
-        app.MapGet("/categoria", () => new CategoryModel("Categoria 1"));
+        var route = app.MapGroup("/categoria");
+        // app.MapGet("/categoria", () => new CategoryModel("Categoria 1"));
+        route.MapPost("", async (CategoryRequest req, CategoryContext context) =>
+        {
+            var category = new CategoryModel(req.Name);
+            await context.AddAsync(category);
+            await context.SaveChangesAsync();
+        });
     }
 }
