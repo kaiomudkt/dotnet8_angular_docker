0378bcc5ef4040a9edf2485882f30a6282dcbf8b
kaio <kaiomudkt@gmail.com>
2025-08-16 23:23:10 -0300

    criando forms com bind

--- angular/app_angular/src/app/components/category-form.component/category-form.component.ts
+++ angular/app_angular/src/app/components/category-form.component/category-form.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { BtnPrimary } from "../btn-primary/btn-primary";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'category-form',
-  imports: [BtnPrimary],
+  imports: [BtnPrimary, ReactiveFormsModule],
   templateUrl: './category-form.component.html',
   styleUrl: './category-form.component.css'
 })
 export class CategoryFormComponent {
+  categoryForm!: FormGroup
 
+  constructor() {
+    this.categoryForm = new FormGroup({
+      id: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required])
+    })
+  }
+
+  onSubmit() {
+    console.log(this.categoryForm.value)
+  }
 }
